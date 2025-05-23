ada440304c98b1134947fd3ef07834ec37604faa
yesh01b <yeswanthb40@gmail.com>
2025-05-23 15:25:54 +0530

    updated

--- Azure/standard/main.tf
+++ Azure/standard/main.tf
@@ -9,6 +9,6 @@ terraform {
     resource_group_name  = "rg-backend-dev-uks-01"
     storage_account_name = "saasbackenddevuks01"
     container_name       = "terraformstate"
-    key                 = "terraform.tfstate"
+    key                 = "terraform"
   }
 }
